89fc87e34041c7afb3158d4d50db0dc6784d97b8
Pablo Soares <pablodacostasoares@gmail.com>
2025-08-04 21:07:35 +0100

    Create ViradoNoJiraya.java

---
+++ ViradoNoJiraya.java
@@ -0,0 +1,6 @@
+public class ViradoNoJiraya {
+    public static void main(String[] args) {
+        int minhaIdade = 31;
+        System.out.println("Eu tenho " + minhaIdade + " Anos!");
+    }
+ }
